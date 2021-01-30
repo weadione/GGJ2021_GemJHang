b2cfc3c785dfa0d2d86eca145b784ff0eb4e7bd6
weadione <36260010+weadione@users.noreply.github.com>
2021-01-30 21:05:10 +0900

    바톤터치

---
+++ Assets/MainGame/Scripts/Player/Bullet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject effect;
+
+    private void Start()
+    {
+        //Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            LivingEntity target = collision.GetComponent<LivingEntity>();
+            EnemyState enemyState = collision.GetComponent<EnemyState>();
+            target.OnDamage(PlayerState.Instance.attDamage);
+            enemyState.HitDetect(0);
+            effect.SetActive(true);
+        }
+
+        if(collision.tag != "Dead")
+            Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
