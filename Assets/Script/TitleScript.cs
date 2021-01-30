1879dbb9df58561e180e12f5f50bddb5aedb551a
CultureAddiction <didqhtmd@gmail.com>
2021-01-30 14:29:27 +0900

    210130 SceneChange01
    scene 변환 초기버전

---
+++ Assets/Script/TitleScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScript : MonoBehaviour
+{
+    public string SceneToLoad;
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(SceneToLoad);
+    }
+
+    public void LoadConfig()
+    {
+        SceneManager.LoadScene(SceneToLoad);
+    }
+
+}
