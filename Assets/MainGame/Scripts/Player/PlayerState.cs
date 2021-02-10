0860ccc41e734d68a4f12257c0a898ec2fd32ca2
weadione <36260010+weadione@users.noreply.github.com>
2021-02-10 19:17:53 +0900

    21.02.10 패치
    1. 루팅Panel 쪽 hierarchy 수정(같은 파츠끼리 묶음)
    2. 루팅 시 파츠 능력치에 따라 능력치 상중하 표시기능 추가
    3. 루팅 시 몬스터가 가지고 있지 않은 파츠는 빼고 정렬해서 표시하게 수정
    4. 최종 보스 버그 아직 수정 중

--- Assets/MainGame/Scripts/Player/PlayerState.cs
+++ Assets/MainGame/Scripts/Player/PlayerState.cs
@@ -35,6 +35,8 @@ public class PlayerState : LivingEntity
     public float machineAdaptationTmp;
 
 
+    
+
     private static PlayerState instance;
     public static PlayerState Instance
     {
--- Assets/Script/LootingUI.cs
+++ Assets/Script/LootingUI.cs
@@ -11,6 +11,21 @@ public class LootingUI : MonoBehaviour
     public GameObject arm;
     public GameObject leg;
 
+    public GameObject descHead;
+    public GameObject descArm;
+    public GameObject descLeg;
+
+    public GameObject[] healthArrows;
+    public GameObject[] attArrows;
+    public GameObject[] attSpeedArrows;
+    public GameObject[] attRangeArrows;
+    public GameObject[] moveSpeedArrows;
+
+    public GameObject headPanel;
+    public GameObject armPanel;
+    public GameObject legPanel;
+
+
     public Sprite noImage;
 
 
@@ -29,39 +44,138 @@ public class LootingUI : MonoBehaviour
         this.itemList = itemlist;
         this.monsterBody = monsterBody;
 
-
+        //머리 파츠 관련 출력
         if (!(itemlist[0] == 0))
         {
+
             //head.SetActive(true);
-            
+            //descHead.SetActive(true);
+            headPanel.SetActive(true);
+
+            //체력 상중하 표시
+            if (partsManager.headParts[itemlist[0]].partsHealth<=50)
+            {
+                healthArrows[1].SetActive(false);
+                healthArrows[2].SetActive(false);
+            }
+            else if(partsManager.headParts[itemlist[0]].partsHealth > 50 && partsManager.headParts[itemlist[0]].partsHealth <= 75)
+            {
+                healthArrows[1].SetActive(true);
+                healthArrows[2].SetActive(false);
+            }
+            else
+            {
+                healthArrows[1].SetActive(true);
+                healthArrows[2].SetActive(true);
+            }
+
+            //파츠 이미지 출력
             head.GetComponent<Image>().sprite = partsManager.headList[itemlist[0]].GetComponent<SpriteRenderer>().sprite;
         }
         else
         {
-            head.GetComponent<Image>().sprite = noImage;
+            headPanel.SetActive(false);
+            //head.SetActive(false);
+            //descHead.SetActive(false);
+            //head.GetComponent<Image>().sprite = noImage;
         }
 
+        //팔 파츠 관련 출력
         if (!(itemlist[1] == 0))
         {
             //arm.SetActive(true);
+            //descArm.SetActive(true);
+            armPanel.SetActive(true);
+
+            //공격력 상중하 표시
+            if(partsManager.armParts[itemlist[1]].partsDamage<=20)
+            {
+                attArrows[1].SetActive(false);
+                attArrows[2].SetActive(false);
+            }
+            else if (partsManager.armParts[itemlist[1]].partsDamage > 20 && partsManager.armParts[itemlist[1]].partsDamage <= 40)
+            {
+                attArrows[1].SetActive(true);
+                attArrows[2].SetActive(false);
+            }
+            else
+            {
+                attArrows[1].SetActive(true);
+                attArrows[2].SetActive(true);
+            }
+
+            //공속 상중하 표시
+            if (partsManager.armParts[itemlist[1]].partsAttSpeed >= 0.5f)
+            {
+                attSpeedArrows[1].SetActive(false);
+                attSpeedArrows[2].SetActive(false);
+            }
+            else if (partsManager.armParts[itemlist[1]].partsAttSpeed <= 0.4f && partsManager.armParts[itemlist[1]].partsAttSpeed >= 0.3f)
+            {
+                attSpeedArrows[1].SetActive(true);
+                attSpeedArrows[2].SetActive(false);
+            }
+            else
+            {
+                attSpeedArrows[1].SetActive(true);
+                attSpeedArrows[2].SetActive(true);
+            }
+
+            //사거리 상중하 표시
+            if (partsManager.armParts[itemlist[1]].partsType)   //근거리(기본팔이 '하'임)
+            {
+                attRangeArrows[1].SetActive(true);
+                attRangeArrows[2].SetActive(false);
+            }
+            else
+            {
+                attRangeArrows[1].SetActive(true);
+                attRangeArrows[2].SetActive(true);
+            }
+
 
             arm.GetComponent<Image>().sprite = partsManager.armList[itemlist[1]].GetComponent<SpriteRenderer>().sprite;
         }
         else
         {
+
+            armPanel.SetActive(false);
             //arm.SetActive(false);
-            arm.GetComponent<Image>().sprite = noImage;
+            //descArm.SetActive(false);
+            //arm.GetComponent<Image>().sprite = noImage;
         }
 
+        //다리 파츠 관련 출력
         if (!(itemlist[2] == 0))
         {
+            legPanel.SetActive(true);
             //leg.SetActive(true);
-            
+            //descLeg.SetActive(true);
+
+            if (partsManager.legParts[itemlist[2]].partsMoveSpeed <= 6)
+            {
+                moveSpeedArrows[1].SetActive(false);
+                moveSpeedArrows[2].SetActive(false);
+            }
+            else if(partsManager.legParts[itemlist[2]].partsMoveSpeed >= 7 && partsManager.legParts[itemlist[2]].partsMoveSpeed <= 8)
+            {
+                moveSpeedArrows[1].SetActive(true);
+                moveSpeedArrows[2].SetActive(false);
+            }
+            else
+            {
+                moveSpeedArrows[1].SetActive(true);
+                moveSpeedArrows[2].SetActive(true);
+            }
+
             leg.GetComponent<Image>().sprite = partsManager.legList[itemlist[2]].GetComponent<SpriteRenderer>().sprite;
         }
         else
         {
-            leg.GetComponent<Image>().sprite = noImage;
+            legPanel.SetActive(false);
+            //leg.SetActive(false);
+            //descLeg.SetActive(false);
+            //leg.GetComponent<Image>().sprite = noImage;
         }
 
         can = true;
