e161328cef9ab788de0ee0d8c50698d9cd868906
CultureAddiction <didqhtmd@gmail.com>
2021-02-11 16:28:30 +0900

    Update UI_HP.cs
    확인 완료

--- Assets/Script/UI_HP.cs
+++ Assets/Script/UI_HP.cs
@@ -26,8 +26,8 @@ public class UI_HP : MonoBehaviour
         HHPMAX =PlayerState.Instance.GetComponent<PartsManager>().headParts[PlayerState.Instance.partsNum[0]].partsHealth;
 
         transform.GetChild(0).GetComponent<Image>().fillAmount = PHP/PHPMAX;
-        transform.GetChild(1).GetComponent<Text>().text = string.Format("HP "+ PHP +"/" + PHPMAX);
+        transform.GetChild(1).GetComponent<Text>().text = string.Format("Player HP "+ PHP +"/" + PHPMAX);
         transform.GetChild(2).GetChild(0).GetComponent<Image>().fillAmount = HHP/HHPMAX;
-        transform.GetChild(2).GetChild(1).GetComponent<Text>().text = string.Format("HHP "+ HHP +"/" + HHPMAX);
+        transform.GetChild(2).GetChild(1).GetComponent<Text>().text = string.Format("Head HP "+ HHP +"/" + HHPMAX);
     }
 }
