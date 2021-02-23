8aee1244453ba9fdca8cb07c44d0362d0a46f93a
dolppe <35285591+dolppe@users.noreply.github.com>
2021-02-23 23:00:43 +0900

    이벤트 스크립트 정리(테스트만 하면 긑)
    이벤트 스크립트 정리(테스트만 하면 긑)

--- Assets/MainGame/Scripts/Event/NextScript.cs
+++ Assets/MainGame/Scripts/Event/NextScript.cs
@@ -8,35 +8,43 @@ using UnityEngine.SceneManagement;
 
 public class NextScript : MonoBehaviour
 {
+    public EventManager EM;
+
+
     public void ScriptPass()
     {
 
-        if (EventManager.Instance.GetComponent<EventManager>().selectStep == 0)
+        if (EM.selectStep == 0)
         {
-            EventManager.Instance.GetComponent<EventManager>().scriptNum = 1;
+            EM.scriptNum = 1;
         }
-        else if (EventManager.Instance.GetComponent<EventManager>().selectStep == 1)
+        else if (EM.selectStep == 1)
         {
          //   Debug.Log("스크립트를 넘깁니다.");
-            EventManager.Instance.GetComponent<EventManager>().scriptNum = 2;
+            EM.scriptNum = 2;
+            Debug.Log(EM.scriptNum);
         }
-        else if (EventManager.Instance.GetComponent<EventManager>().selectStep == 2)
+        else if (EM.selectStep == 2)
         {
-            EventManager.Instance.GetComponent<EventManager>().scriptNum = 3;
+            EM.scriptNum = 3;
         }
-        else if (EventManager.Instance.GetComponent<EventManager>().selectStep == 3)
+        else if (EM.selectStep == 3)
         {
             Debug.Log("스크립트를 넘깁니다.");
-            EventManager.Instance.GetComponent<EventManager>().scriptNum = 4;
+            EM.scriptNum = 4;
         }
-        else if (EventManager.Instance.GetComponent<EventManager>().selectStep == 4)
+        else if (EM.selectStep == 4)
         {
-            EventManager.Instance.GetComponent<EventManager>().scriptNum = 5;
+            EM.scriptNum = 5;
         }
-        else if (EventManager.Instance.GetComponent<EventManager>().selectStep == 5)
+        else if (EM.selectStep == 5)
         {
-            EventManager.Instance.GetComponent<EventManager>().scriptNum = 6;
+            EM.scriptNum = 6;
         }
-        
+        else if (EM.selectStep == 6)
+        {
+            EM.scriptNum = 7;
+        }
+
     }
 }
